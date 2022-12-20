f9f807612c152c788809a63c9985c37af38b2446
Rencist <dafianakhdan@gmail.com>
2022-12-20 16:57:42 +0700

    feat: add fakultas, departemen, wilayah administratif migration

---
+++ database/migrations/2022_12_20_095032_create_kabupaten_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kabupaten', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('provinsi_id')->index();
+            $table->string('name', 128);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->foreign('provinsi_id')->references('id')->on('provinsi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kabupaten');
+    }
+};
---
+++ database/migrations/2022_12_20_095114_create_desa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('desa', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('kecamatan_id')->index();
+            $table->string('name', 128);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->foreign('kecamatan_id')->references('id')->on('kecamatan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('desa');
+    }
+};
