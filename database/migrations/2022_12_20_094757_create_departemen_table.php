d67e8ac3aa91d7d783c9f13d67b6311d8a1bf817
Rencist <dafianakhdan@gmail.com>
2022-12-20 16:57:42 +0700

    feat: add fakultas, departemen, wilayah administratif migration

---
+++ database/migrations/2022_12_20_094757_create_departemen_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departemen', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('fakultas_id')->index();
+            $table->string('name', 128);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->foreign('fakultas_id')->references('id')->on('fakultas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departemen');
+    }
+};
---
+++ database/migrations/2022_12_20_095104_create_kecamatan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kecamatan', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('kabupaten_id')->index();
+            $table->string('name', 128);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            
+            $table->foreign('kabupaten_id')->references('id')->on('kabupaten');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kecamatan');
+    }
+};
