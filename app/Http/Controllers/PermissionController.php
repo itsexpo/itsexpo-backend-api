71f94e05d4db3af510f527ebade80f9055489028
WIN-11 <sclous2012@gmail.com>
2023-01-22 12:24:15 +0700

    feat: add RoleController and PermissionController Files

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Throwable;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Throwable;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+
+class RoleController extends Controller
+{
+    
+}
