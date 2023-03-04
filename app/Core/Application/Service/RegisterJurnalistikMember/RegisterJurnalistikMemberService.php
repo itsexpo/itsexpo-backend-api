15156e83571babe7028ea5fd6dcddbf49cd4f071
zemetia <sclous2012@gmail.com>
2023-03-04 20:14:31 +0700

    feat: add ImageUpload class to check and easily manage the file path, url, verification | fix: Register JurnalistikMemberService to use the ImageUpload class (all previous script are commented)

--- app/Core/Application/Service/RegisterJurnalistikMember/RegisterJurnalistikMemberService.php
+++ app/Core/Application/Service/RegisterJurnalistikMember/RegisterJurnalistikMemberService.php
@@ -2,6 +2,7 @@
 
 namespace App\Core\Application\Service\RegisterJurnalistikMember;
 
+use App\Core\Application\ImageCheck\ImageUpload;
 use App\Core\Domain\Models\Jurnalistik\JurnalistikMemberType;
 use App\Core\Domain\Models\Jurnalistik\Member\JurnalistikMember;
 use App\Core\Domain\Models\UserAccount;
@@ -31,27 +32,47 @@ class RegisterJurnalistikMemberService
         }
 
         // Cek File Exception
-        if ($request->getIdCard()->getSize() > 1048576) {
-            UserException::throw("ID Card Harus Dibawah 1Mb", 2000);
-        }
-        $idCardUrl = Storage::putFileAs('jurnalistik/id_card', $request->getIdCard(), "id_card_".$account->getUserId()->toString());
-        if (!$idCardUrl) {
-            UserException::throw("Upload ID Card Gagal", 2003);
-        }
-        if ($request->getFollowSosmedUrl()->getSize() > 1048576) {
-            UserException::throw("Follow Sosmed Harus Dibawah 1Mb", 2000);
-        }
-        $followUrl = Storage::putFileAs('jurnalistik/follow_sosmed', $request->getFollowSosmedUrl(), "follow_sosmed_".$account->getUserId()->toString());
-        if (!$followUrl) {
-            UserException::throw("Upload Follow Url Card Gagal", 2003);
-        }
-        if ($request->getSharePosterUrl()->getSize() > 1048576) {
-            UserException::throw("Share Sosmed Harus Dibawah 1Mb", 2000);
-        }
-        $shareUrl = Storage::putFileAS('jurnalistik/share_poster', $request->getSharePosterUrl(), "share_poster_".$account->getUserId()->toString());
-        if (!$shareUrl) {
-            UserException::throw("Upload Share Sosmed Gagal", 2003);
-        }
+        $idCardUrl = ImageUpload::create(
+            $request->getIdCard(), 
+            'jurnalistik/id_card', 
+            $account->getUserId()->toString(), 
+            "id_card_")
+                ->upload();
+
+        $followUrl = ImageUpload::create(
+            $request->getFollowSosmedUrl(), 
+            'jurnalistik/follow_sosmed', 
+            $account->getUserId()->toString(), 
+            "Follow Sosmed")
+                ->upload();
+
+        $shareUrl = ImageUpload::create(
+            $request->getSharePosterUrl(), 
+            'jurnalistik/share_poster', 
+            $account->getUserId()->toString(), 
+            "Share Sosmed")
+                ->upload();
+        // if ($request->getIdCard()->getSize() > 1048576) {
+        //     UserException::throw("ID Card Harus Dibawah 1Mb", 2000);
+        // }
+        // $idCardUrl = Storage::putFileAs('jurnalistik/id_card', $request->getIdCard(), "id_card_".$account->getUserId()->toString());
+        // if (!$idCardUrl) {
+        //     UserException::throw("Upload ID Card Gagal", 2003);
+        // }
+        // if ($request->getFollowSosmedUrl()->getSize() > 1048576) {
+        //     UserException::throw("Follow Sosmed Harus Dibawah 1Mb", 2000);
+        // }
+        // $followUrl = Storage::putFileAs('jurnalistik/follow_sosmed', $request->getFollowSosmedUrl(), "follow_sosmed_".$account->getUserId()->toString());
+        // if (!$followUrl) {
+        //     UserException::throw("Upload Follow Url Card Gagal", 2003);
+        // }
+        // if ($request->getSharePosterUrl()->getSize() > 1048576) {
+        //     UserException::throw("Share Sosmed Harus Dibawah 1Mb", 2000);
+        // }
+        // $shareUrl = Storage::putFileAS('jurnalistik/share_poster', $request->getSharePosterUrl(), "share_poster_".$account->getUserId()->toString());
+        // if (!$shareUrl) {
+        //     UserException::throw("Upload Share Sosmed Gagal", 2003);
+        // }
 
         // Create Member
         $member = JurnalistikMember::create(
