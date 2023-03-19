5acf7e0cbaf0e4c76f9f246dd03216ef4ce097ba
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-19 16:40:07 +0700

    fix: fixing permission robotik

--- app/Http/Routes/api.php
+++ app/Http/Routes/api.php
@@ -80,10 +80,10 @@ Route::middleware(['iam'])->group(
         Route::get('/pre_event/pembayaran/jurnalistik', [JurnalistikController::class, 'cekPembayaranJurnalistik'])->middleware('permission:pembayaran_jurnalistik.index');
 
         // Robot In Action
-        Route::post('/pre_event/robotik', [RobotInActionController::class, 'register'])->middleware('permission:robot_in_action.store');
-        Route::get('/pre_event/robotik', [RobotInActionController::class, 'get'])->middleware('permission:robot_in_action.index');
-        Route::post('/pre_event/robotik/pembayaran', [RobotInActionController::class, 'createPembayaran'])->middleware('permission:robot_in_action.createPembayaran');
-        Route::get('/pre_event/robotik/pembayaran', [RobotInActionController::class, 'cekPembayaran'])->middleware('permission:robot_in_action.cekPembayaran');
+        Route::post('/pre_event/robotik', [RobotInActionController::class, 'register'])->middleware('permission:robotik.store');
+        Route::get('/pre_event/robotik', [RobotInActionController::class, 'get'])->middleware('permission:robotik.index');
+        Route::post('/pre_event/robotik/pembayaran', [RobotInActionController::class, 'createPembayaran'])->middleware('permission:robotik_pembayaran.store');
+        Route::get('/pre_event/robotik/pembayaran', [RobotInActionController::class, 'cekPembayaran'])->middleware('permission:robotik_pembayaran.index');
 
         // jurnalistik admin
         Route::get('/admin/jurnalistik', [JurnalistikAdminController::class, 'getTeam'])->middleware('permission:admin_jurnalistik.index');
