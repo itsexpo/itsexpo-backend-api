0eafa31809528f5f365e4835c28004c422b0f93f
Rencist <dafianakhdan@gmail.com>
2023-03-28 16:29:14 +0700

    refactor: paginate response get pangumuman

--- app/Core/Application/Service/GetPengumumanList/GetPengumumanRequest.php
+++ app/Core/Application/Service/GetPengumumanList/GetPengumumanRequest.php
@@ -6,11 +6,15 @@ class GetPengumumanRequest
 {
     private mixed $event_id;
     private mixed $pengumuman_id;
+    private ?int $page;
+    private ?int $per_page;
 
-    public function __construct(int $event_id = null, ?string $pengumuman_id = null)
+    public function __construct(int $event_id = null, ?string $pengumuman_id = null, ?int $page, ?int $per_page)
     {
         $this->event_id = $event_id;
         $this->pengumuman_id = $pengumuman_id;
+        $this->page = $page;
+        $this->per_page = $per_page;
     }
 
 
@@ -22,5 +26,21 @@ class GetPengumumanRequest
     public function getPengumumanId(): string | null
     {
         return $this->pengumuman_id;
+    }
+    
+    /**
+     * @return ?int
+     */
+    public function getPage(): ?int
+    {
+        return $this->page;
+    }
+
+    /**
+     * @return ?int
+     */
+    public function getPerPage(): ?int
+    {
+        return $this->per_page;
     }
 }
---
+++ app/Core/Application/Service/GetPengumumanList/GetPengumumanResponse.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Core\Application\Service\GetPengumumanList;
+
+use JsonSerializable;
+use App\Core\Domain\Models\Pengumuman\Pengumuman;
+
+class GetPengumumanResponse implements JsonSerializable
+{
+    private Pengumuman $pengumuman;
+
+    /**
+     * @param Pengumuman $pengumuman
+     */
+    public function __construct(Pengumuman $pengumuman)
+    {
+        $this->pengumuman = $pengumuman;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->pengumuman->getId()->toString(),
+            'list_event_id' => $this->pengumuman->getListEventId(),
+            'title' => $this->pengumuman->getTitle(),
+            'description' => $this->pengumuman->getDescription(),
+        ];
+    }
+}
