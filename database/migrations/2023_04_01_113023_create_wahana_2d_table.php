ec60382ecd4500c4c13dcf715526cc2f44da84e7
yoel-mc <yoel@mulaicoding.id>
2023-04-01 13:25:16 +0700

    feat: add wahana_2d and wahana_3d(not completed) db migration

---
+++ database/migrations/2023_04_01_113023_create_wahana_2d_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wahana_2d', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('pembayaran_id')->nullable();
+            $table->integer('departemen_id')->index();
+            $table->string('name', 512);
+            $table->string('nrp', 512);
+            $table->string('kontak', 512);
+            $table->string('email', 512);
+            $table->boolean('status');
+            $table->string('ktm_url', 512);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->foreign('pembayaran_id')->references('id')->on('pembayaran');
+            $table->foreign('departemen_id')->references('id')->on('departemen');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('robot_in_action_team');
+    }
+};
---
+++ database/migrations/2023_04_01_175424_create_wahana_3d_team_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wahana_3d_team', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('pembayaran_id')->nullable();
+            $table->string('team_name', 512);
+            $table->string('team_code', 512);
+            $table->string('competition_status', 512);
+            $table->text('deskripsi_karya');
+            $table->integer('jumlah_anggota');
+            $table->boolean('team_status');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->foreign('pembayaran_id')->references('id')->on('pembayaran');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wahana_3d_team');
+    }
+};
