64bd2945e4c0919798d64f2bcf7d0b3e4284c6f0
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-04-02 17:35:47 +0700

    feat: add pembayaran response jurnalistik, kti, robotik

--- app/Core/Application/Service/GetRobotInActionAdminDetail/GetRobotInActionAdminDetailService.php
+++ app/Core/Application/Service/GetRobotInActionAdminDetail/GetRobotInActionAdminDetailService.php
@@ -8,6 +8,7 @@ use App\Core\Domain\Models\RobotInAction\Team\RobotInActionTeamId;
 use App\Core\Domain\Repository\StatusPembayaranRepositoryInterface;
 use App\Core\Domain\Repository\RobotInActionTeamRepositoryInterface;
 use App\Core\Domain\Repository\RobotInActionMemberRepositoryInterface;
+use App\Core\Domain\Repository\ListBankRepositoryInterface;
 
 class GetRobotInActionAdminDetailService
 {
@@ -15,13 +16,15 @@ class GetRobotInActionAdminDetailService
     private RobotInActionMemberRepositoryInterface $robot_in_action_member_repository;
     private StatusPembayaranRepositoryInterface $status_pembayaran_repository;
     private PembayaranRepositoryInterface $pembayaran_repository;
+    private ListBankRepositoryInterface $list_bank_repository;
 
-    public function __construct(RobotInActionTeamRepositoryInterface $robot_in_action_team_repository, RobotInActionMemberRepositoryInterface $robot_in_action_member_repository, StatusPembayaranRepositoryInterface $status_pembayaran_repository, PembayaranRepositoryInterface $pembayaran_repository)
+    public function __construct(RobotInActionTeamRepositoryInterface $robot_in_action_team_repository, RobotInActionMemberRepositoryInterface $robot_in_action_member_repository, StatusPembayaranRepositoryInterface $status_pembayaran_repository, PembayaranRepositoryInterface $pembayaran_repository, ListBankRepositoryInterface $list_bank_repository)
     {
         $this->robot_in_action_team_repository = $robot_in_action_team_repository;
         $this->robot_in_action_member_repository = $robot_in_action_member_repository;
         $this->status_pembayaran_repository = $status_pembayaran_repository;
         $this->pembayaran_repository = $pembayaran_repository;
+        $this->list_bank_repository = $list_bank_repository;
     }
 
     public function execute(string $team_id): GetRobotInActionAdminDetailResponse
@@ -59,8 +62,15 @@ class GetRobotInActionAdminDetailService
             $payment = $this->pembayaran_repository->find($payment_id);
             $payment_status = $this->status_pembayaran_repository->find($payment->getStatusPembayaranId())->getStatus();
             $payment_image_url = $payment->getBuktiPembayaranUrl();
+            $payment_atas_nama = $payment->getAtasNama();
+            if ($payment->getStatusPembayaranId() == 5) {
+                $payment_bank = null;
+            } else {
+                $payment_bank = $this->list_bank_repository->find($payment->getListBankId())->getName();
+            }
+            $payment_harga = $payment->getHarga();
 
-            $payment_obj = new PembayaranObjResponse($payment_status, $payment_id->toString(), $payment_image_url);
+            $payment_obj = new PembayaranObjResponse($payment_status, $payment_id->toString(), $payment_image_url, $payment_atas_nama, $payment_bank, $payment_harga);
 
             $final = new GetRobotInActionAdminDetailResponse($team->getTeamName(), $team->getTeamCode(), $payment_obj, $member_array);
             return $final;
