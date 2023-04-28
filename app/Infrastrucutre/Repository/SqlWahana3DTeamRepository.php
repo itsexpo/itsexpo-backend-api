ae35047553146296e077b419bcb2685e2402f161
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-04-28 21:22:20 +0700

    fix: status_pengumpulan and order_by

--- app/Infrastrucutre/Repository/SqlWahana3DTeamRepository.php
+++ app/Infrastrucutre/Repository/SqlWahana3DTeamRepository.php
@@ -26,7 +26,7 @@ class SqlWahana3DTeamRepository implements Wahana3DTeamRepositoryInterface
     public function findByPembayaranId(PembayaranId $pembayaran_id): ?Wahana3DTeam
     {
         $row = DB::table('wahana_3d_team')->where('pembayaran_id', $pembayaran_id->toString())->first();
-        
+
         if (!$row) {
             return null;
         }
@@ -63,7 +63,7 @@ class SqlWahana3DTeamRepository implements Wahana3DTeamRepositoryInterface
 
     public function getTeams(): Builder
     {
-        $rows = DB::table('wahana_3d_team')->leftJoin('pembayaran', 'wahana_3d_team.pembayaran_id', '=', 'pembayaran.id')->leftJoin('status_pembayaran', 'pembayaran.status_pembayaran_id', '=', 'status_pembayaran.id')->leftJoin('wahana_3d_member', 'wahana_3d_team.id', '=', 'wahana_3d_member.wahana_3d_team_id')->where('wahana_3d_member.member_type', 'KETUA');
+        $rows = DB::table('wahana_3d_team')->leftJoin('pembayaran', 'wahana_3d_team.pembayaran_id', '=', 'pembayaran.id')->leftJoin('status_pembayaran', 'pembayaran.status_pembayaran_id', '=', 'status_pembayaran.id')->leftJoin('wahana_3d_member', 'wahana_3d_team.id', '=', 'wahana_3d_member.wahana_3d_team_id')->where('wahana_3d_member.member_type', 'KETUA')->orderBy('wahana_3d_team.created_at', 'desc');
 
         if (!$rows) {
             return null;
@@ -90,7 +90,7 @@ class SqlWahana3DTeamRepository implements Wahana3DTeamRepositoryInterface
         if (!$rows) {
             return 0;
         }
-      
+
         return $rows;
     }
 
@@ -101,32 +101,32 @@ class SqlWahana3DTeamRepository implements Wahana3DTeamRepositoryInterface
         if (!$rows) {
             return 0;
         }
-      
+
         return $rows;
     }
 
     public function getFilter(Builder $wahana_team, ?array $filter): void
     {
-        $wahana_team->where('pembayaran.status_pembayaran_id', $filter);
+        $wahana_team->where('pembayaran.status_pembayaran_id', $filter)->orderBy('wahana_3d_team.created_at', 'desc');
     }
 
     public function getSearch(Builder $wahana_team, ?string $search): void
     {
-        $wahana_team->where('wahana_3d_team.team_name', 'like', '%' . $search . '%')->orWhere('wahana_3d_member.name', 'like', '%' . $search . '%');
+        $wahana_team->where('wahana_3d_team.team_name', 'like', '%' . $search . '%')->orWhere('wahana_3d_member.name', 'like', '%' . $search . '%')->orderBy('wahana_3d_team.created_at', 'desc');
     }
 
     public function persist(Wahana3DTeam $team): void
     {
         DB::table('wahana_3d_team')->upsert([
-          'id' => $team->getId()->toString(),
-          'pembayaran_id' => $team->getPembayaranId()->toString(),
-          'user_id' => $team->getUserId()->toString(),
-          'team_name' => $team->getTeamName(),
-          'team_code' => $team->getTeamCode(),
-          'deskripsi_karya' => $team->getDeskripsiKarya(),
-          'upload_karya_url' => $team->getUploadKaryaUrl(),
-          'deskripsi_url' => $team->getDeskripsiUrl(),
-          'form_keaslian_url' => $team->getFormKeaslianUrl(),
+            'id' => $team->getId()->toString(),
+            'pembayaran_id' => $team->getPembayaranId()->toString(),
+            'user_id' => $team->getUserId()->toString(),
+            'team_name' => $team->getTeamName(),
+            'team_code' => $team->getTeamCode(),
+            'deskripsi_karya' => $team->getDeskripsiKarya(),
+            'upload_karya_url' => $team->getUploadKaryaUrl(),
+            'deskripsi_url' => $team->getDeskripsiUrl(),
+            'form_keaslian_url' => $team->getFormKeaslianUrl(),
         ], 'id');
     }
 
